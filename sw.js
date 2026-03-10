fd2e1185439328895c85dd3dd34f3956800cdbe5
pabloff2007-arch <pabloff2007@gmail.com>
2026-03-10 12:37:52 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'misiones-v1';
+const ASSETS = [
+  '/misiones/',
+  '/misiones/index.html',
+  '/misiones/manifest.json',
+  '/misiones/icon-192.png',
+  '/misiones/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
